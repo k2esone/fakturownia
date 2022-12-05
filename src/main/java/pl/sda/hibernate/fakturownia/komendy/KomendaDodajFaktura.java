034a77213b9e8043db265bd5214b825d66ec8865
Dominik Kowalczyk <dkowalczyk.lwc@gmail.com>
2022-12-05 20:11:35 +0100

    fix issues

--- src/main/java/pl/sda/hibernate/fakturownia/komendy/KomendaDodajFaktura.java
+++ src/main/java/pl/sda/hibernate/fakturownia/komendy/KomendaDodajFaktura.java
@@ -3,13 +3,17 @@ package pl.sda.hibernate.fakturownia.komendy;
 import pl.sda.hibernate.fakturownia.database.DataAccessObject;
 import pl.sda.hibernate.fakturownia.model.Faktura;
 import pl.sda.hibernate.fakturownia.model.Firma;
+import pl.sda.hibernate.fakturownia.model.Kontrahent;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class KomendaDodajFaktura implements Komenda {
 
     private DataAccessObject<Faktura> dao = new DataAccessObject<Faktura>();
+    private DataAccessObject<Firma> daoFirma = new DataAccessObject<>();
+    private DataAccessObject<Kontrahent> daoKontrahent = new DataAccessObject<>();
 
     @Override
     public String getKomenda() {
@@ -18,6 +22,23 @@ public class KomendaDodajFaktura implements Komenda {
 
     @Override
     public void obsluga() {
+        System.out.println("Podaj id firmy:");
+        String idFirmyString = Komenda.scanner.nextLine();
+        Long idFirmy = Long.parseLong(idFirmyString);
+        Optional<Firma> firmaOptional = daoFirma.find(Firma.class, idFirmy);
+        if (firmaOptional.isEmpty()) {
+            System.err.println("Firma o podanym id nie istnieje");
+            return;
+        }
+
+        System.out.println("Podaj id kontrahenta:");
+        String idKontrahentaString = Komenda.scanner.nextLine();
+        Long idKontrahenta = Long.parseLong(idKontrahentaString);
+        Optional<Kontrahent> kontrahentOptional = daoKontrahent.find(Kontrahent.class, idKontrahenta);
+        if (kontrahentOptional.isEmpty()) {
+            System.err.println("Kontrahent o podanym id nie istnieje");
+            return;
+        }
         System.out.println("Podaj numer faktury");
         String numer = Komenda.scanner.nextLine();
 
@@ -31,6 +52,8 @@ public class KomendaDodajFaktura implements Komenda {
         Double kwota = Double.parseDouble(kwotaString);
 
         Faktura faktura = Faktura.builder()
+                .kontrahent(kontrahentOptional.get())
+                .firma(firmaOptional.get())
                 .numerFaktury(numer)
                 .terminPlatnosci(termin)
                 .kwota(kwota)
